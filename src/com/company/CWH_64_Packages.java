7e500083e3cd3f722696762c1748e2045ec58dfb
Sheetal Kabra <sheetal.kabra@eduvanz.in>
2021-12-26 08:55:56 +0530

    packages complted till video 65

--- src/com/company/CWH_64_Packages.java
+++ src/com/company/CWH_64_Packages.java
@@ -7,4 +7,14 @@ public class CWH_64_Packages {
 //        3. Two types of packages we have
 //            1. Built in package --- Java API
 //            2. User defined package --- Custom package
+
+//    javac -d . *.java ---- this will put all the files of java into their particular packages.
+//    javac -d . harry.java ---- this will put only harry.java file into their particular packages.
+        //. means wahi p ban jaye package
+//    we can include package like -> package codewithharry
+//    we can import java package, with 3 types:
+//    1. import java.util.Scanner;
+//    2. import java.util.*; -- import everything
+//    3. java.util.Scanner sc = new java.util.Scanner(System.in); --- use without importing
+
 }
