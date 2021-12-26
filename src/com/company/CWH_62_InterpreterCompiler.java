8f856619ab8bb72d5627bc10b003a442fc66781d
Sheetal Kabra <sheetal.kabra@eduvanz.in>
2021-12-26 08:03:15 +0530

    video 62 and 63- interpreter and complier

---
+++ src/com/company/CWH_62_InterpreterCompiler.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CWH_62_InterpreterCompiler {
+//    Interpreter: it translates one line at a time into machine code.
+//    1. it is needed everytime
+//    2.Partial execution if error
+//    3. Easy for programmers, java python
+//    4. slower
+//    Complier: It scans the entire program and translates it into machine code
+//    1. Once complied, not needed
+//    2. No execution if error coours
+//    3. usually not easy,
+//    4. faster
+
+//    Java is hybrid lang - means both complier and interpreter
+//      java file(harry.java) ----------------------->Class file(harry.class) -----------> can be used by java interpreter
+//                            complied(using javac)           (byte code)
+//                                                                |
+//                                                    (This is platform independent. This can be run anywhere like linux)
+//                                                            We can use any JVM
+//    Hence java is platform independent.(write once run everywhere)
+// Executing java file
+    //1. javac harry.java
+    //2. java harry.class
+//for executing java, we need to add the java path into environment variables.
+
+
+
+}
